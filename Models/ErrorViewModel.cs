97fe49afe3f086b8701ad36a6615836b1a455cf2
NiqueWrld <siyathokoza321@gmail.com>
2024-09-04 21:46:39 +0200

    updat2

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Sbahle_Events.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
